0c918fcb3da70d9fd9de354b7ee7fdcd2922303f
7wai7 <nazarkalataluk@gmail.com>
2026-03-26 12:32:30 +0200

    feat: downloading audio

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,40 @@
-import "dotenv/config"
-import { Bot } from "grammy";
+import { spawn } from "child_process";
+import { Bot, InputFile } from "grammy";
+import fs from "fs";
+import "dotenv/config";
 
 const bot = new Bot(process.env.TG_TOKEN!);
 
-bot.on("message", (ctx) => {
-    ctx.reply("Hi there!")
+bot.on("message", async (ctx) => {
+    const url = ctx.message.text;
+    const file = `audio_${Date.now()}.mp3`;
+
+    if (!url?.includes("youtube.com") && !url?.includes("youtu.be")) {
+        return ctx.reply("Send YouTube URL");
+    }
+
+    try {
+        await new Promise((resolve, reject) => {
+            const proc = spawn("yt-dlp", [
+                "-x",
+                "--audio-format", "mp3",
+                "--no-playlist",
+                "-o", file,
+                url!
+            ]);
+
+            proc.on("close", (code) => {
+                if (code === 0) resolve(null);
+                else reject(new Error("yt-dlp failed"));
+            });
+        });
+
+        await ctx.replyWithAudio(new InputFile(file));
+
+        fs.unlinkSync(file);
+    } catch {
+        ctx.reply("Error processing video");
+    }
 });
 
 bot.start();
