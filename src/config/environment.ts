8fd5aefbea999b80253b5616ab2f41721ee95ea6
7wai7 <nazarkalataluk@gmail.com>
2026-04-01 11:16:09 +0300

    dev token and allowed users

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,6 +1,9 @@
 import "dotenv/config";
 
 export const environment = {
+    NODE_ENV: process.env.NODE_ENV!,
+    WEBHOOK_URL: process.env.WEBHOOK_URL!,
     TG_TOKEN: process.env.TG_TOKEN!,
+    TG_TOKEN_DEV: process.env.TG_TOKEN_DEV!,
     YT_COOKIES_BASE64: process.env.YT_COOKIES_BASE64!
 }
---
+++ src/constants/index.ts
@@ -0,0 +1,4 @@
+// users who are allowed to use the bot for development
+export const ALLOWED_USERS = new Set<number>([
+  959266994
+]);
