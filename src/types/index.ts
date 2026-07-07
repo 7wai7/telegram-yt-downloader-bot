ae21f2e08f409356d42dd1a869ca7a3810e99808
7wai7 <nazarkalataluk@gmail.com>
2026-07-07 22:07:14 +0300

    add settings

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,7 +2,13 @@ export type MediaType = "audio" | "video";
 
 export type DownloadState = {
     id: string
+    userId: number,
     url: string;
     type?: MediaType;
     splitChapters?: boolean;
+    waitingForTimeRange?: boolean;
+    timeRange?: {
+        start: string;
+        end: string;
+    };
 };
--- src/utils/handleDownload.ts
+++ src/utils/handleDownload.ts
@@ -27,6 +27,14 @@ export default async function handleDownload(ctx: Context, state: DownloadState)
         "-o", "chapter:chapters/%(section_title)s.%(ext)s",
     ];
 
+    const timeRangeArgs = state.timeRange
+        ? [
+            "--download-sections",
+            `*${state.timeRange.start}-${state.timeRange.end}`,
+            "--force-keyframes-at-cuts",
+        ]
+        : [];
+
     try {
         if (state.splitChapters) {
             const data = await runYtDlp(["--dump-json", ...baseArgs, state.url]);
@@ -50,6 +58,7 @@ export default async function handleDownload(ctx: Context, state: DownloadState)
                 "--embed-thumbnail",
 
                 ...baseArgs,
+                ...timeRangeArgs,
                 ...(state.splitChapters ? splitChaptersArgs : singleDownloadArgs),
                 state.url
             ]);
@@ -72,8 +81,10 @@ export default async function handleDownload(ctx: Context, state: DownloadState)
         } else if (state.type === "video") {
             await runYtDlp([
                 "-f", "mp4",
-                "--dump-json", "--no-simulate",
+                "--dump-json",
+                "--no-simulate",
                 ...baseArgs,
+                ...timeRangeArgs,
                 ...(state.splitChapters ? splitChaptersArgs : singleDownloadArgs),
                 state.url
             ]);
@@ -88,12 +99,11 @@ export default async function handleDownload(ctx: Context, state: DownloadState)
         } else {
             await ctx.reply("Invalid type");
         }
-
-        cleanupTmp(path);
     } catch (e) {
-        cleanupTmp(path);
         await ctx.reply("Something went wrong");
         throw e;
+    } finally {
+        void cleanupTmp(path);
     }
 }
 
